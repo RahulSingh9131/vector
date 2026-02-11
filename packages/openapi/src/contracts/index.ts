be5dd2792193beb640eeb073a1ef3d4f67390906
RahulSingh9131 <singh.rahul3111998vipin@gmail.com>
2026-02-11 14:06:49 +0530

    fix(user): route prefix for openapi.json file

--- packages/openapi/src/contracts/index.ts
+++ packages/openapi/src/contracts/index.ts
@@ -5,8 +5,18 @@ import { organizationContract } from "./organization.js";
 
 const c = initContract();
 
+const apiV1 = c.router(
+    {
+        User: userContract,
+        Organization: organizationContract,
+    },
+    {
+        pathPrefix: "/api/v1",
+    }
+);
+
 export const apiContract = c.router({
     Health: healthContract,
-    User: userContract,
-    Organization: organizationContract,
+    User: apiV1.User,
+    Organization: apiV1.Organization,
 });
